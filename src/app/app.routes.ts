1972e00fe19ab0ea6adafc06b4d23c51348fb800
Sanzhar Irgebay <theoneeetheonlyyy@gmail.com>
2026-05-28 00:12:04 +0500

    fix-remarks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,25 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('../home-page/home-page.component').then(module => module.HomePageComponent) },
-  { path: 'users-page', loadComponent: () => import('../users-page/users-page.component').then(module => module.UsersPageComponent) },
-  { path: '**', loadComponent: () => import('../not-found-page/not-found-page.component').then(module => module.NotFoundPageComponent) },
+  {
+    path: '',
+    loadComponent: () =>
+      import('../home-page/home-page.component').then(
+        (module) => module.HomePageComponent
+      ),
+  },
+  {
+    path: 'users-page',
+    loadComponent: () =>
+      import('../users-page/users-page.component').then(
+        (module) => module.UsersPageComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('../not-found-page/not-found-page.component').then(
+        (module) => module.NotFoundPageComponent
+      ),
+  },
 ];
--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -3,12 +3,12 @@ import { Observable, tap } from 'rxjs';
 
 export const LoggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next:HttpHandlerFn): Observable<HttpEvent<unknown>> => {
 
-  const currentTime: number = Date.now();
+  const requestTime: number = Date.now();
   return next(req)
     .pipe(
       tap((event: HttpEvent<unknown>) => {
         if (event instanceof HttpResponse) {
-          console.log(req.method, req.url, event.status, Date.now() - currentTime);
+          console.log(req.method, req.url, event.status, Date.now() - requestTime);
         }
       })
     )
