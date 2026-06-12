0a18d11e68aacd4e8a948927028fd0bfe3efe042
Sanzhar Irgebay <theoneeetheonlyyy@gmail.com>
2026-06-12 23:34:43 +0500

    fix-remarks-4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,19 @@ import { postResolver } from '../features/posts/post.resolver';
 
 export const routes: Routes = [
   {
-    path: 'posts',
-    loadComponent: () => import('../features/posts/posts/posts.component').then(module => module.PostsComponent),
-    children: [
-      {
-        path: 'create',
-        loadComponent: () => import('../features/posts/post-create/post-create.component').then(module => module.PostCreateComponent),
-      }
-    ],
+    path: 'posts/create',
+    loadComponent: () => import('../features/posts/post-create/post-create.component').then(module => module.PostCreateComponent),
+  },
+  {
+    path: 'posts/:id',
+    loadComponent: () => import('../features/posts/post-detail/post-detail.component').then(module => module.PostDetailComponent),
+    resolve: {
+      post: postResolver
+    },
   },
   {
     path: 'posts',
     loadComponent: () => import('../features/posts/posts/posts.component').then(module => module.PostsComponent),
-    children: [
-      {
-        path: ':id',
-        loadComponent: () => import('../features/posts/post-detail/post-detail.component').then(module => module.PostDetailComponent),
-        resolve: {
-          post: postResolver
-        },
-      }
-    ],
   },
   {
     path: 'users-page',
